ac5b8dadde90aa6aba5a8b2571db26bb2a186f19
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2020-07-05 10:42:51 +0200

    re-naming from boilerplate

--- src/content.js
+++ src/content.js
@@ -7,11 +7,11 @@ import App from "./App";
 class Main extends React.Component {
     render() {
         return (
-            <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}> 
+            <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}>
                <FrameContextConsumer>
                {
                   ({document, window}) => {
-                    return <App document={document} window={window} isExt={true}/> 
+                    return <App document={document} window={window} isExt={true}/>
                   }
                 }
                 </FrameContextConsumer>
@@ -21,7 +21,7 @@ class Main extends React.Component {
 }
 
 const app = document.createElement('div');
-app.id = "my-extension-root";
+app.id = "formkeeper-root";
 
 document.body.appendChild(app);
 ReactDOM.render(<Main />, app);
