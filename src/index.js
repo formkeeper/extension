44d0442bc1a802101de173d7f62257077ae4c626
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2020-07-05 14:36:36 +0200

    add note to src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// This file is remnant piece of the create-react-app boilerplate. Useful for developing UI in development mode with hot reloading
+// but completely useless within chrome extension development.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
