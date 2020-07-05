19128241938409aaf52d10386fa57330d7f3180b
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2020-07-05 17:35:53 +0200

    remove isExt prop from App

--- src/App.js
+++ src/App.js
@@ -4,16 +4,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App ({ isExt }) {
+function App () {
   return (
     <div className="App">
       <header className="App-header">
-        {isExt ?
-          <img src={chrome.runtime.getURL("static/media/logo.svg")} className="App-logo" alt="logo" />
-        :
-          <img src={logo} className="App-logo" alt="logo" />
-        }
-
+        <img src={chrome.runtime.getURL("static/media/logo.svg")} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
       </header>
       <p className="App-intro">
