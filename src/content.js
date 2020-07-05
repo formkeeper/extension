1bd7c2bfb7c98d5d63239dd9c28df077a2872cc0
Pedro Martínez Martínez <pedrojmrt@gmail.com>
2020-07-05 17:35:31 +0200

    Main is now a pure comp

--- src/content.js
+++ src/content.js
@@ -4,20 +4,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Frame, { FrameContextConsumer }from 'react-frame-component';
 import App from "./App";
-class Main extends React.Component {
-    render() {
-        return (
-            <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}>
-               <FrameContextConsumer>
-               {
-                  ({document, window}) => {
-                    return <App document={document} window={window} isExt={true}/>
-                  }
-                }
-                </FrameContextConsumer>
-            </Frame>
-        )
-    }
+
+function Main() {
+  return (
+      <Frame head={[<link type="text/css" rel="stylesheet" href={chrome.runtime.getURL("/static/css/content.css")} ></link>]}>
+         <FrameContextConsumer>
+         {
+            ({document, window}) => {
+              return <App document={document} window={window}/>
+            }
+          }
+          </FrameContextConsumer>
+      </Frame>
+  )
 }
 
 const app = document.createElement('div');
